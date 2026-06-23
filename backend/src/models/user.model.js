16cf4761eed03d0aeec8044423f3b3d3c2b88975
lakshminiranjan <lakshmi.niranjan2019@vitstudent.ac.in>
2026-06-23 16:04:46 +0530

    auth implemented

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,67 @@
+import mongoose from 'mongoose';
+
+const addressSchema = new mongoose.Schema({
+  label: {
+    type: String,
+    required: true
+  },
+  fullName: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  streetAddress: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  postalCode: {
+    type: String,
+    required: true
+  },
+  isDefault: {
+    type: Boolean,
+    default: false
+  }
+
+});
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  imageUrl: {
+    type: String,
+    default: ''
+  },
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  addresses: [addressSchema],
+  wishlist: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product'
+    }
+  ],
+}, { timestamps: true });
+
+export const User = mongoose.model('User', userSchema);
