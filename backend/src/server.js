7f688006ff62d2d77d7417fcbdd121ea2e57e5ed
lakshminiranjan <lakshmi.niranjan2019@vitstudent.ac.in>
2026-06-24 10:55:29 +0530

    cart routes and controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,8 @@ import userRoutes from './routes/user.route.js';
 import orderRoutes from "./routes/order.route.js";
 import reviewRoutes from "./routes/review.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import cors from "cors";
 
 const app = express();
 
@@ -17,12 +19,16 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(clerkMiddleware())//register Clerk middleware to handle authentication
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+
+
 app.use("/api/inngest", serve({ client: inngest, functions }))//register Inngest middleware to handle incoming events
 app.use("/api/admin", adminRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
