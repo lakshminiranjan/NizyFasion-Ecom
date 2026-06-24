7a62a3b91d01cdad46190ba149e653a254da11ec
lakshminiranjan <lakshmi.niranjan2019@vitstudent.ac.in>
2026-06-24 12:35:06 +0530

    admin dashboard tools setup

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,23 +1,38 @@
 import React from 'react'
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Show, SignInButton, SignUpButton, useAuth, UserButton } from '@clerk/react'
+import { useQuery } from '@tanstack/react-query'
+import { Navigate, Route, Routes } from 'react-router'
+import LoginPage from './pages/LoginPage'
+import DashboardPage from './pages/DashboardPage.jsx'
+import ProductsPage from './pages/ProductsPage'
+import OrdersPage from './pages/OrdersPage'
+import CustomersPage from './pages/CustomersPage'
+import DashboardLayout from './layouts/DashboardLayout.jsx'
+import PageLoader from './components/PageLoader.jsx'
 
 function App() {
-  return (
-    <div>
-      <header>
-        <h1>
-          Welcome to Clerk + Vite + React!
-        </h1>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
-  )
+
+  const { isSignedIn, isLoaded } = useAuth()
+
+  if (!isLoaded) return <PageLoader />;
+
+
+
+
+    return (
+    <Routes>
+      <Route path='/login' element={isSignedIn ? <Navigate to={"/dashboard"} /> : <LoginPage />} />
+
+      <Route path='/' element={isSignedIn ? <DashboardLayout /> : <Navigate to={"/login"} />}>
+        <Route index element={<Navigate to={"/dashboard"} />} />
+        <Route path='dashboard' element={<DashboardPage />} />
+        <Route path='products' element={<ProductsPage />} />
+        <Route path='orders' element={<OrdersPage />} />
+        <Route path='customers' element={<CustomersPage />} />
+
+      </Route>
+    </Routes>
+    )
 }
 
-export default App
+    export default App
---
+++ admin/src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import { SignIn } from '@clerk/react'
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>LoginPage
+
+      <SignIn />
+    </div>
+  )
+}
+
+export default LoginPage
